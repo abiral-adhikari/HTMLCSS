fe06fa97b9992e35566f7fcdad794a4dd3e24058
abiral-adhikari <109904208+abiral-adhikari@users.noreply.github.com>
2023-08-18 08:03:46 +0545

    Time Button MouseEvents

---
+++ script.js
@@ -0,0 +1,55 @@
+
+const time=()=>{
+    const currentTime=new Date()
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+    var currenttime="";
+    if(hours>12){
+        currenttime = `${hours-12}:${minutes}:${seconds} pm`;
+    }else{
+        currenttime = `${hours-12}:${minutes}:${seconds} am`;
+    }
+    var Text=document.getElementsByClassName("Time")[0]
+    if(Text.textContent=="Time"){
+        Text.textContent=currenttime
+        Text.style.backgroundColor="lightgreen"
+        Text.style.borderRadius="1px"
+    }
+    else
+    {
+        Text.textContent="Time"
+        Text.style.backgroundColor="tomato"
+        Text.style.borderRadius="100%"
+    }
+}
+
+ const hover=()=>{
+    document.getElementsByClassName("Time")[0].style.backgroundColor="orange"
+    document.getElementsByClassName("Time")[0].style.borderRadius="20px"
+ }
+
+ const reset=()=>{
+    const currentTime=new Date()
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+    var currenttime="";
+    if(hours>12){
+        currenttime = `${hours-12}:${minutes}:${seconds} pm`;
+    }else{
+        currenttime = `${hours-12}:${minutes}:${seconds} am`;
+    }
+    var Text=document.getElementsByClassName("Time")[0]
+    if(Text.textContent!="Time"){
+        Text.textContent=currenttime
+        Text.style.backgroundColor="lightgreen"
+        Text.style.borderRadius="1px"
+    }
+    else
+    {
+        Text.textContent="Time"
+        Text.style.backgroundColor="tomato"
+        Text.style.borderRadius="100%"
+    }
+}
